7934d06bdd22b29342f97f1d6ef70b9f6adcab18
konan080211 <konanforbis@gmail.com>
2023-08-20 08:03:20 +0900

    fix: 環境変数化

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,18 @@
 from flask import Flask
 import mysql.connector
+import os
 
 app = Flask(__name__)
+
 
 def get_db():
     """データベース接続を取得する関数"""
     return mysql.connector.connect(
-        host="",
-        user="",
-        password="",
-        database=""
+        host=os.getenv("DB_HOST", "db"),
+        user=os.getenv("DB_USER", "admin"),
+        password=os.getenv("DB_PASSWORD", "pass"),
+        database=os.getenv("DB_NAME", "taskfracture")
     )
+
 
 from app import routes

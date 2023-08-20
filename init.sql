6073a35c40ad2199e302b14f0a8e59c0c10cc0bd
konan080211 <konanforbis@gmail.com>
2023-08-20 22:23:15 +0900

    orderカラムの追加

--- init.sql
+++ init.sql
@@ -5,6 +5,7 @@ CREATE TABLE `tasks` (
   `actual_hours` float DEFAULT NULL COMMENT '実績工数',
   `status` int(11) NOT NULL COMMENT '0: Not Started, 1: In Progress, 2: Completed',
   `parent_task_id` int(11) DEFAULT NULL COMMENT '親タスクのID',
+  `order` int(11) DEFAULT NULL COMMENT 'タスクの順序',
   PRIMARY KEY (`id`),
   KEY `parent_task_id` (`parent_task_id`),
   CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`parent_task_id`) REFERENCES `tasks` (`id`)

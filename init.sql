ec5ac4d8573052d3b96b5bb292d33ec3f12aed63
konan080211 <konanforbis@gmail.com>
2023-08-20 09:15:24 +0900

    update

--- init.sql
+++ init.sql
@@ -1,9 +1,11 @@
-CREATE TABLE tasks (
-    id              INT          AUTO_INCREMENT PRIMARY KEY COMMENT 'ユニークなタスクID',
-    name            VARCHAR(255)                            COMMENT 'タスク名',
-    estimated_hours FLOAT                                   COMMENT '見積もり工数',
-    actual_hours    FLOAT                                   COMMENT '実績工数',
-    status          INT          NOT NULL                   COMMENT '0: Not Started, 1: In Progress, 2: Completed',
-    parent_task_id  INT                                     COMMENT '親タスクのID',
-    FOREIGN KEY(parent_task_id) REFERENCES tasks(id)
-) CHARACTER SET utf8mb4 COMMENT='タスクの情報';
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ユニークなタスクID',
+  `name` varchar(255) DEFAULT NULL COMMENT 'タスク名',
+  `estimated_hours` float DEFAULT NULL COMMENT '見積もり工数',
+  `actual_hours` float DEFAULT NULL COMMENT '実績工数',
+  `status` int(11) NOT NULL COMMENT '0: Not Started, 1: In Progress, 2: Completed',
+  `parent_task_id` int(11) DEFAULT NULL COMMENT '親タスクのID',
+  PRIMARY KEY (`id`),
+  KEY `parent_task_id` (`parent_task_id`),
+  CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`parent_task_id`) REFERENCES `tasks` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COMMENT='タスクの情報';

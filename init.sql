58c5a657d908c46ab2bacd02c19ea9f16f52732e
konan080211 <konanforbis@gmail.com>
2023-08-21 20:22:15 +0900

    sqlの修正

--- init.sql
+++ init.sql
@@ -1,6 +1,18 @@
-CREATE TABLE `tasks` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ユニークなタスクID',
-  `name` varchar(255) DEFAULT NULL COMMENT 'タスク名',
+-- 親タスクを管理するテーブル
+CREATE TABLE `parent_tasks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ユニークな親タスクID',
+  `name` varchar(255) DEFAULT NULL COMMENT '親タスク名',
+  `estimated_hours` float DEFAULT NULL COMMENT '見積もり工数',
+  `actual_hours` float DEFAULT NULL COMMENT '実績工数',
+  `status` int(11) NOT NULL COMMENT '0: Not Started, 1: In Progress, 2: Completed',
+  `order` int(11) DEFAULT NULL COMMENT 'タスクの順序',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='親タスクの情報';
+
+-- 子タスクを管理するテーブル
+CREATE TABLE `sub_tasks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ユニークな子タスクID',
+  `name` varchar(255) DEFAULT NULL COMMENT '子タスク名',
   `estimated_hours` float DEFAULT NULL COMMENT '見積もり工数',
   `actual_hours` float DEFAULT NULL COMMENT '実績工数',
   `status` int(11) NOT NULL COMMENT '0: Not Started, 1: In Progress, 2: Completed',
@@ -8,5 +20,5 @@ CREATE TABLE `tasks` (
   `order` int(11) DEFAULT NULL COMMENT 'タスクの順序',
   PRIMARY KEY (`id`),
   KEY `parent_task_id` (`parent_task_id`),
-  CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`parent_task_id`) REFERENCES `tasks` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COMMENT='タスクの情報';
+  CONSTRAINT `sub_tasks_ibfk_1` FOREIGN KEY (`parent_task_id`) REFERENCES `parent_tasks` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='子タスクの情報';

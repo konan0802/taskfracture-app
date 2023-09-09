21e40af4f84554fdc3e46b7873995a81d1d25cf6
konan080211 <konanforbis@gmail.com>
2023-09-09 19:46:27 +0900

    Enterによる親タスクの新規作成

--- frontend/src/ParentTask.js
+++ frontend/src/ParentTask.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { ReactSortable } from 'react-sortablejs';
 import ChildTask from './ChildTask';
 
-export default function ParentTask({ task }) {
+export default function ParentTask({ task, addParentTask, index }) {
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      addParentTask(null, index + 1);
+    }
+    // Additional key events can be added here later
+  };
+
   return (
     <li className="task-item">
       <div className="task-parent-div">
-        <input value={task.name} placeholder='Task Name' ></input>
+        <input value={task.name} placeholder='Task Name' onKeyDown={handleKeyDown}></input>
       </div>
       <ReactSortable className="task-child-list" list={task.children} setList={(newChildren) => ({ ...task, children: newChildren })}>
         {task.children.map((childTask) => (
--- frontend/src/TaskList.js
+++ frontend/src/TaskList.js
@@ -6,16 +6,18 @@ export default function TaskList() {
   const [parentTasks, setParentTasks] = useState([]);
   const [taskIdCounter, setTaskIdCounter] = useState(0);
 
-  const addParentTask = (name) => {
+  const addParentTask = (name, index = parentTasks.length) => {
     const newTaskId = taskIdCounter + 1;
     const newTask = {
       id: newTaskId,
       name,
       isParent: true,
-      order: parentTasks.length,
+      order: index,
       children: []
     };
-    setParentTasks([...parentTasks, newTask]);
+    const newParentTasks = [...parentTasks];
+    newParentTasks.splice(index, 0, newTask);
+    setParentTasks(newParentTasks);
     setTaskIdCounter(newTaskId);
   };
 
@@ -38,8 +40,8 @@ export default function TaskList() {
 
   return (
     <ReactSortable id="task-parent-list" list={parentTasks} setList={setParentTasks}>
-      {parentTasks.map((task) => (
-        <ParentTask key={task.id} task={task} />
+      {parentTasks.map((task, index) => (
+        <ParentTask key={task.id} task={task} addParentTask={addParentTask} index={index} />
       ))}
     </ReactSortable>
   );

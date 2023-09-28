3b8549cc44af22901c2d53be2b5c107f088c1124
konan080211 <konanforbis@gmail.com>
2023-09-28 19:44:25 +0900

    fix: 繋ぎこみ罐量

--- backend/resources/views/app.blade.php
+++ backend/resources/views/app.blade.php
@@ -4,11 +4,12 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" />
     <title>My React App</title>
     <link href="{{ asset('main.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app"></div>
+    <div id="root"></div>
     <script src="{{ asset('main.js') }}"></script>
 </body>
 </html>
